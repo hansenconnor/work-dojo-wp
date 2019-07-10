c8d8156db3505d21757293cf4394e376adb11640
hansenconnor <connorhnsn5596@gmail.com>
2019-07-10 17:09:09 -0400

    Add  inside header styles

--- wp-content/themes/relationship-works/templates/header-inside.php
+++ wp-content/themes/relationship-works/templates/header-inside.php
@@ -1,4 +1,4 @@
-<section class="header-inside pb-0">
+<section class="header-inside pb-0" style="background: url('/wp-content/uploads/2019/07/accent-curved-gradient.png'); background-position: bottom; background-size: contain; background-repeat: no-repeat;">
     <div class="container">
         <div class="row">
             <div class="col-12 col-lg-6 offset-lg-3 text-center">
@@ -7,5 +7,5 @@
             </div>
         </div>
     </div>
-    <figure class="m-0"><img src="/wp-content/uploads/2019/07/accent-curved-gradient.png" class="img-fluid" alt="curved orange accent"></figure>
+    <!-- <figure class="m-0"><img src="/wp-content/uploads/2019/07/accent-curved-gradient.png" class="img-fluid" alt="curved orange accent"></figure> -->
 </section>
